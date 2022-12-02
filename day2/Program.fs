e761221e20453c36838ef97646ea48a7c7cbafc0
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2022-12-02 11:50:47 +0100

    Solve day 2, puzzle 2

--- day2/Program.fs
+++ day2/Program.fs
@@ -14,6 +14,7 @@ type Move = Shape
 type Response = Shape
 type Round = Move * Response
 type Game = Shape * Outcome
+type Strategem = Move * Outcome
 
 let outcome (round: Round) =
     match round with
@@ -35,6 +36,23 @@ let gameScore outcome =
     | Draw -> 3
     | Lose -> 0
 
+let determineRound ((move, outcome): Strategem) : Round =
+    let response =
+        match outcome with
+        | Draw -> move
+        | Win ->
+            match move with
+            | Rock -> Paper
+            | Paper -> Scissor
+            | Scissor -> Rock
+        | Lose ->
+            match move with
+            | Rock -> Scissor
+            | Paper -> Rock
+            | Scissor -> Paper
+
+    (move, response)
+
 let score (shape, outcome) =
     (gameScore outcome) + (shapeScore shape)
 
@@ -53,11 +71,31 @@ let charToShape ch =
     | 'Z' -> Scissor
     | _ -> failwith (sprintf "Unsupported move '%c'" ch)
 
+let charToExpectedOutcome ch =
+    match ch with
+    | 'X' -> Lose
+    | 'Y' -> Draw
+    | 'Z' -> Win
+    | _ -> failwith (sprintf "Unsupported outcome '%c'" ch)
+
+
 let stringToRound (s: string) : Round = (charToShape s[0], charToShape s[2])
+
+let stringToStrategem (s: string) : Strategem =
+    (charToShape s[0], charToExpectedOutcome s[2])
+
 
 File.ReadLines("./day2/input.txt")
 |> Seq.map stringToRound
 |> Seq.map play
 |> Seq.map score
 |> Seq.sum
-|> printfn "%A"
+|> printfn "Total score, misunderstood strategy: %d"
+
+File.ReadLines("./day2/input.txt")
+|> Seq.map stringToStrategem
+|> Seq.map determineRound
+|> Seq.map play
+|> Seq.map score
+|> Seq.sum
+|> printfn "Total score, correct strategy: %d"
