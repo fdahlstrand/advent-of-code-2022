94c83eae1ea7b2bcd7c6611b39a9fbf2118f0b5f
Fredrik Dahlstrand <fredrik.dahlstrand@outlook.com>
2022-12-03 19:39:54 +0100

    Refactor more of the solution for day 3

--- day3/Program.fs
+++ day3/Program.fs
@@ -1,33 +1,43 @@
 ﻿open System.IO
 
+type InventoryList = string list
+type Item = char
+
+module InventoryList =
+    let fromFile (path: string): InventoryList = File.ReadLines(path) |> List.ofSeq
+   
+    let private findCommonItem (bag: string) =
+        (bag
+         |> Seq.chunkBySize (bag.Length / 2)
+         |> Seq.map Set.ofArray
+         |> Set.intersectMany
+         |> Set.toArray)[0]
+
+    let findCommonItemPerBag (list: InventoryList): Item list =
+        list |> List.map findCommonItem
+        
+    let collectGroupsBySize (groupSize: int) (list: InventoryList): InventoryList list =
+        list |> List.chunkBySize groupSize
+        
+    let findCommonBadgeItemType (list: InventoryList) =
+        (list |> List.map Set.ofSeq |> Set.intersectMany |> Set.toArray)[0]
+
 let calculatePriority (ch: char) =
     match ch with
     | c when 'a' <= c && c <= 'z' -> int c - int 'a' + 1
     | c when 'A' <= c && c <= 'Z' -> int c - int 'A' + 27
     | _ -> failwithf $"Unsupported bag item '%c{ch}'"
 
-let findCommonItem (bag: string) =
-    (bag
-     |> Seq.chunkBySize (bag.Length / 2)
-     |> Seq.map Set.ofArray
-     |> Set.intersectMany
-     |> Set.toArray)[0]
-
 let findCommonBadgeItemType (group: Set<char> []) =
     (group |> Set.intersectMany |> Set.toArray)[0]
 
-let collectGroupsBySize (groupSize: int) (list: string seq) =
-    list |> Seq.map Set.ofSeq |> Seq.chunkBySize groupSize
-
-File.ReadLines("./day3/input.txt")
-|> Seq.map findCommonItem
-|> Seq.map calculatePriority
-|> Seq.sum
+InventoryList.fromFile("./day3/input.txt")
+|> InventoryList.findCommonItemPerBag
+|> List.sumBy calculatePriority
 |> printfn "Total priority of bag items %d"
 
-File.ReadLines("./day3/input.txt")
-|> collectGroupsBySize 3
-|> Seq.map findCommonBadgeItemType
-|> Seq.map calculatePriority
-|> Seq.sum
+InventoryList.fromFile("./day3/input.txt")
+|> InventoryList.collectGroupsBySize 3
+|> List.map InventoryList.findCommonBadgeItemType
+|> List.sumBy calculatePriority
 |> printfn "Total priority of badge item types %d"
