9fdadf4f1053301cb2a78ccd2ef6c3b411f69adb
Fredrik Dahlstrand <fredrik.dahlstrand@outlook.com>
2022-12-03 17:42:22 +0100

    Clean up solution for day 3

--- day3/Program.fs
+++ day3/Program.fs
@@ -1,38 +1,33 @@
 ﻿open System.IO
 
-let stringToSet (str: string) =
-    (Set.empty<char>, str) ||> Seq.fold (fun s -> s.Add)
-
-module Bag =
-    let parse (str: string) =
-            
-        let midpoint = str.Length/2
-        (stringToSet str[..midpoint-1], stringToSet str[midpoint..])
-
-let calculatePriority (ch:char) =
+let calculatePriority (ch: char) =
     match ch with
     | c when 'a' <= c && c <= 'z' -> int c - int 'a' + 1
     | c when 'A' <= c && c <= 'Z' -> int c - int 'A' + 27
     | _ -> failwithf $"Unsupported bag item '%c{ch}'"
 
+let findCommonItem (bag: string) =
+    (bag
+     |> Seq.chunkBySize (bag.Length / 2)
+     |> Seq.map Set.ofArray
+     |> Set.intersectMany
+     |> Set.toArray)[0]
+
+let findCommonBadgeItemType (group: Set<char> []) =
+    (group |> Set.intersectMany |> Set.toArray)[0]
+
+let collectGroupsBySize (groupSize: int) (list: string seq) =
+    list |> Seq.map Set.ofSeq |> Seq.chunkBySize groupSize
+
 File.ReadLines("./day3/input.txt")
-|> Seq.map (
-    fun str -> str
-               |> Bag.parse
-               ||> Set.intersect
-               |> Set.map calculatePriority
-               |> Set.toList
-               |> List.sum)
+|> Seq.map findCommonItem
+|> Seq.map calculatePriority
 |> Seq.sum
 |> printfn "Total priority of bag items %d"
 
 File.ReadLines("./day3/input.txt")
-|> Seq.map stringToSet
-|> Seq.chunkBySize 3
-|> Seq.map (fun group -> group
-                         |> Set.intersectMany
-                         |> Set.map calculatePriority
-                         |> Set.toList
-                         |> List.sum )
+|> collectGroupsBySize 3
+|> Seq.map findCommonBadgeItemType
+|> Seq.map calculatePriority
 |> Seq.sum
 |> printfn "Total priority of badge item types %d"
