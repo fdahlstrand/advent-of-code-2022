303ca5325d766e56d4c83b8acaecf8ba0434c4c9
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2022-12-05 16:24:54 +0100

    Solve day 5, puzzle 1

--- day5/Program.fs
+++ day5/Program.fs
@@ -1,8 +1,13 @@
-﻿open System.Text.RegularExpressions
-
-type Crate = char
+﻿type Crate = char
 type Stack = Crate list
 type Ship = Map<int, Stack>
+
+type Instruction =
+    { Quantity: int
+      Source: int
+      Target: int }
+
+type Procedure = Instruction list
 
 module Crate =
     let tryParse (str: string) : Crate option =
@@ -13,6 +18,7 @@ module Crate =
 
 module Ship =
     open System.IO
+    open System.Text.RegularExpressions
 
     type private CrateRow = (int * Crate option) []
 
@@ -56,56 +62,47 @@ module Ship =
 
     let fromFile = File.ReadAllLines >> ofSeq
 
+    let rec move ship instruction =
+        if (instruction.Quantity = 0) then
+            ship
+        else
+            let newShip =
+                match Map.find instruction.Source ship with
+                | x :: xs ->
+                    ship
+                    |> Map.add instruction.Source xs
+                    |> Map.add instruction.Target (x :: (Map.find instruction.Target ship))
+                | [] -> failwith "Try to collect from empty stack"
 
-// [<Literal>]
-// let movePattern =
-//     @"^move\s(?<count>[1-9][0-9]*)\sfrom\s(?<source>[1-9][0-9]*)\sto\s(?<target>[1-9][0-9]*)"
+            move newShip { instruction with Quantity = instruction.Quantity - 1 }
 
+module Procedure =
+    open System
+    open System.IO
+    open System.Text.RegularExpressions
 
+    [<Literal>]
+    let private movePattern =
+        @"^move\s(?<count>[1-9][0-9]*)\sfrom\s(?<source>[1-9][0-9]*)\sto\s(?<target>[1-9][0-9]*)"
 
-// let (|CrateMap|_|) input =
-//     let m = Regex.Match(input, cratePattern)
+    let private (|Instruction|_|) input =
+        let m = Regex.Match(input, movePattern)
 
-//     if (m.Success) then
-//         let crates =
-//             [| for i in 0 .. m.Groups.["slot"].Captures.Count - 1 -> Crate.tryParse m.Groups.["slot"].Captures.[i].Value |]
-//             |> Array.indexed
-//             |> Array.map (fun (ix, c) -> (ix + 1, c))
+        if (m.Success) then
+            Some
+                { Quantity = Int32.Parse(m.Groups.["count"].Value)
+                  Source = Int32.Parse(m.Groups.["source"].Value)
+                  Target = Int32.Parse(m.Groups.["target"].Value) }
+        else
+            None
 
-//         Some crates
-//     else
-//         None
+    let parseRow (row: string) =
+        match row with
+        | Instruction i -> Some i
+        | _ -> None
 
-// let (|MoveInstruction|_|) input =
-//     let m = Regex.Match(input, movePattern)
-
-//     if (m.Success) then
-//         Some m.Value
-//     else
-//         None
-
-
-
-// let stacks: Crate list list = []
-
-// let folder (acc: Map<int, Crate list>) (row: (int * Crate option) []) =
-//     let innerFold (a: Map<int, Crate list>) (ix: int, c: Crate option) =
-//         match c with
-//         | Some x when Map.containsKey ix a -> Map.add ix ((Map.find ix a) @ [ x ]) a
-//         | Some x -> Map.add ix [ x ] a
-//         | None -> a
-
-//     Array.fold innerFold acc row
-
-
-// sampleData
-// |> Seq.map (fun s ->
-//     match s with
-//     | CrateMap crates -> Some crates
-//     | _ -> None)
-// |> Seq.choose id
-// |> Seq.fold folder Map.empty<int, Crate list>
-// |> printfn "%A"
+    let ofSeq: string seq -> Procedure = Seq.map parseRow >> Seq.choose id >> Seq.toList
+    let fromFile = File.ReadAllLines >> ofSeq
 
 let sampleData =
     seq {
@@ -120,6 +117,31 @@ let sampleData =
         "move 1 from 1 to 2"
     }
 
-sampleData |> Ship.ofSeq |> printfn "%A"
+let sampleShip = sampleData |> Ship.ofSeq
 
-Ship.fromFile "./day5/input.txt" |> printf "%A"
+
+sampleData
+|> Procedure.ofSeq
+|> List.fold Ship.move sampleShip
+|> Map.toList
+|> List.map (fun (_, crates) ->
+    match crates with
+    | c :: cs -> c
+    | [] -> ' ')
+|> System.String.Concat
+|> printfn "%s"
+
+
+
+
+let ship = Ship.fromFile "./day5/input.txt"
+
+Procedure.fromFile "./day5/input.txt"
+|> List.fold Ship.move ship
+|> Map.toList
+|> List.map (fun (_, crates) ->
+    match crates with
+    | c :: cs -> c
+    | [] -> ' ')
+|> System.String.Concat
+|> printfn "%s"
