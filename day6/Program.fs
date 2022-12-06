281db879a1932fb467e8e7935520871eb07d9b09
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2022-12-06 09:16:36 +0100

    Solve day 6 puzzles 1 and 2

--- day6/Program.fs
+++ day6/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+type StreamState = { Buffer: string; Pos: int }
+
+let packetizer N state =
+    if (state.Pos + N >= String.length state.Buffer) then
+        None
+    else
+        Some((state.Pos + N, state.Buffer[state.Pos .. state.Pos + (N - 1)]), { state with Pos = state.Pos + 1 })
+
+let countUniqueChars = Set.ofSeq >> Set.count
+
+let detectMarker N str =
+    let gen = packetizer N
+
+    { Buffer = str; Pos = 0 }
+    |> Seq.unfold gen
+    |> Seq.map (fun (p, s) -> (p, countUniqueChars s))
+    |> Seq.filter (fun (p, c) -> c = N)
+    |> Seq.map (fun (p, c) -> p)
+    |> Seq.min
+
+File.ReadAllText("./day6/input.txt")
+|> detectMarker 4
+|> printfn "Start-of-packet marker @%d"
+
+File.ReadAllText("./day6/input.txt")
+|> detectMarker 14
+|> printfn "Start-of-packet marker @%d"
