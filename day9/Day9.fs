4a10bf7ceba2e1f0c091f83b5bafd6cb3b02c4ee
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2022-12-09 15:00:11 +0100

    Solve day 9, puzzle 2

--- day9/Day9.fs
+++ day9/Day9.fs
@@ -18,7 +18,7 @@ let moveHeadStep dir pos =
     | Right -> { pos with X = pos.X + 1 }
 
 let moveTailStep headPos tailPos =
-    let (dx, dy) = diff headPos tailPos
+    let (dx, dy) = diff tailPos headPos
 
     match (dx, dy) with
     | (-1, -1) -> tailPos
@@ -30,40 +30,44 @@ let moveTailStep headPos tailPos =
     | (-1, 1) -> tailPos
     | (0, 1) -> tailPos
     | (1, 1) -> tailPos
-    | (0, dy) when dy = -2 -> { tailPos with Y = tailPos.Y - 1 }
-    | (0, dy) when dy = 2 -> { tailPos with Y = tailPos.Y + 1 }
-    | (dx, 0) when dx = -2 -> { tailPos with X = tailPos.X - 1 }
-    | (dx, 0) when dx = 2 -> { tailPos with X = tailPos.X + 1 }
+    | (0, -2) -> { tailPos with Y = tailPos.Y + 1 }
+    | (0, 2) -> { tailPos with Y = tailPos.Y - 1 }
+    | (-2, 0) -> { tailPos with X = tailPos.X + 1 }
+    | (2, 0) -> { tailPos with X = tailPos.X - 1 }
     | (dx, dy) ->
         match (dx, dy) with
-        | (-2, -1) -> { X = tailPos.X - 1; Y = tailPos.Y - 1 }
-        | (-1, -2) -> { X = tailPos.X - 1; Y = tailPos.Y - 1 }
-        | (2, -1) -> { X = tailPos.X + 1; Y = tailPos.Y - 1 }
-        | (1, -2) -> { X = tailPos.X + 1; Y = tailPos.Y - 1 }
-        | (2, 1) -> { X = tailPos.X + 1; Y = tailPos.Y + 1 }
-        | (1, 2) -> { X = tailPos.X + 1; Y = tailPos.Y + 1 }
-        | (-2, 1) -> { X = tailPos.X - 1; Y = tailPos.Y + 1 }
-        | (-1, 2) -> { X = tailPos.X - 1; Y = tailPos.Y + 1 }
+        | (-2, -1) -> { X = tailPos.X + 1; Y = tailPos.Y + 1 }
+        | (-1, -2) -> { X = tailPos.X + 1; Y = tailPos.Y + 1 }
+        | (2, -1) -> { X = tailPos.X - 1; Y = tailPos.Y + 1 }
+        | (1, -2) -> { X = tailPos.X - 1; Y = tailPos.Y + 1 }
+        | (2, 1) -> { X = tailPos.X - 1; Y = tailPos.Y - 1 }
+        | (1, 2) -> { X = tailPos.X - 1; Y = tailPos.Y - 1 }
+        | (-2, 1) -> { X = tailPos.X + 1; Y = tailPos.Y - 1 }
+        | (-1, 2) -> { X = tailPos.X + 1; Y = tailPos.Y - 1 }
+        | (-2, -2) -> { X = tailPos.X + 1; Y = tailPos.Y + 1 }
+        | (-2, 2) -> { X = tailPos.X + 1; Y = tailPos.Y - 1 }
+        | (2, -2) -> { X = tailPos.X - 1; Y = tailPos.Y + 1 }
+        | (2, 2) -> { X = tailPos.X - 1; Y = tailPos.Y - 1 }
         | _ -> failwith $"No rule for H:%A{headPos}, T:%A{tailPos}, d:%A{(dx, dy)}"
 
 
 type State =
     { HeadPos: Position
-      TailPos: Position
+      Tails: Position list
       TailPositions: Set<Position> }
 
-let startState =
-    { HeadPos = { X = 0; Y = 0 }
-      TailPos = { X = 0; Y = 0 }
-      TailPositions = Set.empty }
+let moveLink headPos tailPos =
+    let newTailPos = moveTailStep headPos tailPos
+
+    newTailPos, newTailPos
 
 let move state dir =
     let headPos = moveHeadStep dir state.HeadPos
-    let tailPos = moveTailStep headPos state.TailPos
+    let tails, lastTail = (headPos, state.Tails) ||> List.mapFold moveLink
 
     { HeadPos = headPos
-      TailPos = tailPos
-      TailPositions = Set.add tailPos state.TailPositions }
+      Tails = tails
+      TailPositions = Set.add lastTail state.TailPositions }
 
 let moveHead state (dir, count) =
     (state,
@@ -74,18 +78,6 @@ let moveHead state (dir, count) =
     ||> Seq.fold move
 
 
-
-let stringData =
-    seq {
-        "R 4"
-        "U 4"
-        "L 3"
-        "D 1"
-        "R 4"
-        "D 1"
-        "L 5"
-        "R 2"
-    }
 
 let parseInstruction str =
     let m = Regex.Match(str, @"^(?<dir>[RULD])\s(?<cnt>[1-9][0-9]*)$")
@@ -110,9 +102,47 @@ let instructionsFromFile =
     System.IO.File.ReadAllLines
     >> instructionsFromStrings
 
+let inputSample1 =
+    seq {
+        "R 4"
+        "U 4"
+        "L 3"
+        "D 1"
+        "R 4"
+        "D 1"
+        "L 5"
+        "R 2"
+    }
+    |> instructionsFromStrings
 
-(startState, instructionsFromFile "./day9/input.txt")
-||> Seq.fold moveHead
-|> (fun s -> s.TailPositions)
-|> Set.count
-|> printfn "%d"
+let inputSample2 =
+    seq {
+        "R 5"
+        "U 8"
+        "L 8"
+        "D 3"
+        "R 17"
+        "D 10"
+        "L 25"
+        "U 20"
+    }
+    |> instructionsFromStrings
+
+let inputFile = instructionsFromFile "./day9/input.txt"
+
+let createState linkLength =
+    { HeadPos = { X = 0; Y = 0 }
+      Tails = List.init linkLength (fun _ -> { X = 0; Y = 0 })
+      TailPositions = Set.empty }
+
+let countTailVisits input linkLength =
+    (createState linkLength, input)
+    ||> Seq.fold moveHead
+    |> (fun s -> s.TailPositions)
+    |> Set.count
+
+countTailVisits inputFile 1
+|> printfn "For a chain with 2 links, the tail visits %d positions."
+
+countTailVisits inputFile 9
+|> printfn "For a chain with 10 links, the tail visits %d positions."
