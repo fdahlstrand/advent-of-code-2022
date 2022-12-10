52c12d9c72a2f9708ae27f9ab050250bdce0a0fb
Fredrik Dahlstrand <fredrik.dahlstrand@outlook.com>
2022-12-10 10:34:10 +0100

    Solve day 10, puzzle 1

--- day10/day10.fs
+++ day10/day10.fs
@@ -1,2 +1,71 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type CPU = {
+    X: int
+    Cycles: int []
+}
+type Instruction = | Noop | Addx of int
+
+type Program = Instruction list
+
+module Instruction =
+    let noop cpu =
+        { X = cpu.X; Cycles = [| yield! cpu.Cycles; cpu.X |]  }
+        
+    let addx cpu v =
+        { X = cpu.X + v; Cycles = [| yield! cpu.Cycles; cpu.X; cpu.X |] }
+
+module CPU =
+    let startState = {
+        X = 1
+        Cycles = [| 1 |]
+    }
+    
+    let execute state instr =
+        match instr with
+        | Noop -> Instruction.noop state
+        | Addx v -> Instruction.addx state v
+        
+    let run program =    
+        (startState, program) ||> List.fold execute |> (fun cpu -> [|yield! cpu.Cycles; cpu.X|])
+        
+module Program =
+    open System
+    open System.IO
+    open System.Text.RegularExpressions
+    let (|Noop|_|) input =
+        let m = Regex.Match(input, "^noop$")
+        
+        if (m.Success) then
+            Some ()
+        else
+            None
+            
+    let (|Addx|_|) input =
+        let m = Regex.Match(input, "^addx\s+(?<v>\-?[0-9]+)$")
+        
+        if (m.Success) then
+            Some (Int32.Parse(m.Groups["v"].Value))
+        else
+            None
+            
+    let parse str =
+        match str with
+        | Noop -> Instruction.Noop
+        | Addx v -> Instruction.Addx v
+        | _ -> failwith $"Unsupported instruction '%s{str}'"
+
+    let fromStrings: string seq -> Instruction list = Seq.map parse >> Seq.toList
+    
+    let fromFile = File.ReadAllLines >> fromStrings
+        
+
+let sample1 = Program.fromFile "./day10/sample1.txt"
+let sample2 = Program.fromFile "./day10/sample2.txt"
+let input = Program.fromFile "./day10/input.txt"
+
+
+let indices = [for i in 20 .. 40 .. 220 -> i]
+
+let d = input |> CPU.run
+[|for i in 20 .. 40 .. 220 -> i*d[i]|] |> Array.sum |> printfn "%A"
+
+
